bb5806704bb47b94a7846ed3616f2a4adda59ef8
Jado <jade.bouiges@edu.itescia.fr>
2024-01-09 16:41:44 +0100

    add part systeme

---
+++ Assets/ControlsManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControlsManager : MonoBehaviour
+{
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}

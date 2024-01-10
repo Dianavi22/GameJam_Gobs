609482e58fc3b1a7d2d16b0b457cc1953d99d26d
Jado <jade.bouiges@edu.itescia.fr>
2024-01-10 13:25:24 +0100

    add effect controlsMenu

--- Assets/ControlsManager.cs
+++ Assets/ControlsManager.cs
@@ -5,8 +5,36 @@ using UnityEngine.SceneManagement;
 
 public class ControlsManager : MonoBehaviour
 {
+
+    [SerializeField] GameObject _effects;
     public void MainMenu()
     {
         SceneManager.LoadScene(1);
+    }
+
+    private void Start()
+    {
+        StartCoroutine(BlinkEffect());
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    IEnumerator  BlinkEffect()
+    {
+        for (int i = 0; i < 10000; i++)
+        {
+            _effects.SetActive(false);
+            yield return new WaitForSeconds(0.5f);
+            _effects.SetActive(true);
+            yield return new WaitForSeconds(0.5f);
+
+            i++;
+        }
+       
+
+        yield return null;
     }
 }
